8369dd7eb454c315aa4aa9286c7bf953446bbbf4
Griezman2003 <garciagomezgamaliel@gmail.com>
2023-10-16 12:06:20 -0600

    implementacion de las cards

--- resources/views/layouts/cuerpo.blade.php
+++ resources/views/layouts/cuerpo.blade.php
@@ -12,7 +12,21 @@
 
 <div class="contenido">
 <div class="container-sm">Contenido</div>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor nam nulla harum quasi expedita accusamus odio mollitia minus, consectetur sapiente totam facilis, omnis nostrum, earum tempore repudiandae aperiam quia provident.</p>
-</div>
+  </section>
 
+  <section class="card">
+  <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md 12">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
   </section>

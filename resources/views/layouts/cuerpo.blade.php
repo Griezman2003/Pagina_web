244219d62532735e4b8740aed3ffd2ce29317972
Griezman2003 <garciagomezgamaliel@gmail.com>
2023-10-16 01:08:48 -0600

    hover

--- resources/views/layouts/cuerpo.blade.php
+++ resources/views/layouts/cuerpo.blade.php
@@ -1,10 +1,10 @@
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img  src="https://static-uat.cambiocolombia.com/s3fs-public/2023-09/equipo-videojuegos-futurista-iluminado-ia-generativa-discoteca.jpg" class="rounded mx-auto d-block">
+        <img src="https://static-uat.cambiocolombia.com/s3fs-public/2023-09/equipo-videojuegos-futurista-iluminado-ia-generativa-discoteca.jpg" class="rounded mx-auto d-block">
       </div>
       <div class="carousel-item">
-        <img src="#" class="mx-auto d-block">
+        <img src="https://media.vandal.net/m/1-2022/202211215412443_1.jpg" class="mx-auto d-block">
       </div>
     </div>
   </div>

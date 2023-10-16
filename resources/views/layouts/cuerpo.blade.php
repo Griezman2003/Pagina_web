f1838485d1875791d019632efe8186d1f2d7e0a0
Griezman2003 <garciagomezgamaliel@gmail.com>
2023-10-15 23:00:34 -0600

    comment

--- resources/views/layouts/cuerpo.blade.php
+++ resources/views/layouts/cuerpo.blade.php
@@ -1,10 +1,10 @@
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="https://static-uat.cambiocolombia.com/s3fs-public/2023-09/equipo-videojuegos-futurista-iluminado-ia-generativa-discoteca.jpg" class="d-block w-100"  alt="..">
+        <img src="https://static-uat.cambiocolombia.com/s3fs-public/2023-09/equipo-videojuegos-futurista-iluminado-ia-generativa-discoteca.jpg" class="img-fluid" width="60%" height="105" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdpGKyqdrw7cdBfmABmzzlxGSZ9inP3YHjsgZglu9j5mgKD3IkMEZI5pwe0ejHg9sA8fM&usqp=CAU" class="d-block w-100" alt="...">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdpGKyqdrw7cdBfmABmzzlxGSZ9inP3YHjsgZglu9j5mgKD3IkMEZI5pwe0ejHg9sA8fM&usqp=CAU" class="img-fluid" width="60%" height="50%" alt="...">
       </div>
     </div>
   </div>
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -4,6 +4,6 @@
 <script src="assets2/js/bootstrap.min.js"></script>
 <script src="assets2/js/main.js"></script>
 
-
+{{-- scripts del cuerpo de la pagina --}}
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>

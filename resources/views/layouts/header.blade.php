027939b6d17884b42fa495939c1cdc793d0addcf
Griezman2003 <garciagomezgamaliel@gmail.com>
2023-10-16 00:45:51 -0600

    mispell

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,19 +1,19 @@
 
 <ul class="nav justify-content-center custom-background">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="{{ route('login') }}">Registro</a>
-  </li>
-  <li class="nav-item">
     <a class="nav-link" href="#">Inicio</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Contacto</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="#">Contenido</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="#">Informacion</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contacto</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="{{ route('registro') }}">Registro</a>
   </li>
 
 </ul>

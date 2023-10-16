df41abad272af8ad1f32e64e1048effddb919c8c
Griezman2003 <garciagomezgamaliel@gmail.com>
2023-10-16 14:55:00 -0600

    imagenes agregadas a las carpetas

--- resources/views/layouts/cuerpo.blade.php
+++ resources/views/layouts/cuerpo.blade.php
@@ -1,16 +1,15 @@
 <section class="carrusel">
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
-      <div class="carousel-item active" data-bs-interval="5000">
+      <div class="carousel-item active" data-bs-interval="4000">
       <img src="https://static-uat.cambiocolombia.com/s3fs-public/2023-09/equipo-videojuegos-futurista-iluminado-ia-generativa-discoteca.jpg" class="rounded mx-auto d-block">
       </div>
-      <div class="carousel-item" data-bs-interval="5000">
+      <div class="carousel-item" data-bs-interval="4000">
       <img src="https://media.vandal.net/m/1-2022/202211215412443_1.jpg" class="mx-auto d-block">
       </div>
     </div>
   </div>
 
-<div class="contenido">
 
 <section class="destacados">
 <div class="container-fluid"><h1 class="titulo">DESTACADOS</h1></div>
@@ -26,7 +25,7 @@
     </div>
   </div>
   <div class="card">
-    <img src="https://i.blogs.es/09c084/roblox-juegos/450_1000.webp" class="card-img-top" alt="...">
+    <img src="https://media.vandal.net/i/640x360/4-2022/202242615281483_1.jpg" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Título de la tarjeta</h5>
       <p class="card-text">Esta tarjeta tiene texto de apoyo a continuación como una introducción natural a contenido adicional.</p>
@@ -34,7 +33,7 @@
     </div>
   </div>
   <div class="card">
-    <img src="https://i.blogs.es/09c084/roblox-juegos/450_1000.webp" class="card-img-top" alt="...">
+    <img src="https://www.mundodeportivo.com/alfabeta/hero/2020/11/Guia-completa-de-League-of-Legends-trucos-y-secretos.jpg?width=768&aspect_ratio=16:9&format=nowebp" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Título de la tarjeta</h5>
       <p class="card-text">Esta es una tarjeta más amplia con texto de apoyo a continuación como introducción natural a contenido adicional. Esta tarjeta tiene un contenido aún más largo que la primera para mostrar esa acción de igual altura.</p>

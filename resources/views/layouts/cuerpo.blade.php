f5fd503d4c66e30f869ab7d483d3179d2f2f237d
Griezman2003 <garciagomezgamaliel@gmail.com>
2023-10-16 15:27:38 -0600

    links modificados

--- resources/views/layouts/cuerpo.blade.php
+++ resources/views/layouts/cuerpo.blade.php
@@ -2,10 +2,16 @@
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active" data-bs-interval="3000">
-      <img src="https://static-uat.cambiocolombia.com/s3fs-public/2023-09/equipo-videojuegos-futurista-iluminado-ia-generativa-discoteca.jpg" class="rounded mx-auto d-block">
+      <img src="https://img.redbull.com/images/c_crop,w_2400,h_1200,x_0,y_274,f_auto,q_auto/c_scale,w_1200/redbullcom/2020/6/2/tjpwxedhwufmcyuuhmpf/playstation-video-juegos" class="rounded mx-auto d-block">
       </div>
       <div class="carousel-item" data-bs-interval="3000">
-      <img src="https://media.vandal.net/m/1-2022/202211215412443_1.jpg" class="mx-auto d-block">
+      <img src="https://cdn.milenio.com/uploads/media/2020/07/22/el-videojuego-saldra-a-finales.jpg" class="mx-auto d-block">
+      </div>
+      <div class="carousel-item" data-bs-interval="3000">
+      <img src="https://i1.wp.com/www.silviagaliana.com/wp-content/uploads/2013/01/Final-Fantasy-VII-Cover-Twitter.jpg" class="mx-auto d-block">
+      </div>
+      <div class="carousel-item" data-bs-interval="3000">
+      <img src="https://img.unocero.com/2020/10/league-of-legends-consolas-1200x600.jpg" class="mx-auto d-block">
       </div>
     </div>
   </div>

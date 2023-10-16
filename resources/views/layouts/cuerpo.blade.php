25e866ef1ca77928b4cf0a4204cd265893836318
Griezman2003 <garciagomezgamaliel@gmail.com>
2023-10-16 15:14:23 -0600

    misspell

--- resources/views/layouts/cuerpo.blade.php
+++ resources/views/layouts/cuerpo.blade.php
@@ -1,10 +1,10 @@
 <section id="carrusel">
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
-      <div class="carousel-item active" data-bs-interval="4000">
+      <div class="carousel-item active" data-bs-interval="3000">
       <img src="https://static-uat.cambiocolombia.com/s3fs-public/2023-09/equipo-videojuegos-futurista-iluminado-ia-generativa-discoteca.jpg" class="rounded mx-auto d-block">
       </div>
-      <div class="carousel-item" data-bs-interval="4000">
+      <div class="carousel-item" data-bs-interval="3000">
       <img src="https://media.vandal.net/m/1-2022/202211215412443_1.jpg" class="mx-auto d-block">
       </div>
     </div>

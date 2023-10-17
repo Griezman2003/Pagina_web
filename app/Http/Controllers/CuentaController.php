ec9bc0837d6b6a0a09d8ff05cc5494447ebdd409
Griezman2003 <garciagomezgamaliel@gmail.com>
2023-10-17 17:21:25 -0600

    rama ya confirmada

--- app/Http/Controllers/CuentaController.php
+++ app/Http/Controllers/CuentaController.php
@@ -1,20 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-<<<<<<< HEAD
-
-use Illuminate\Http\Request;
-use App\Models\Cuenta;
-
-class CuentaController extends Controller
-{
-    public function validate(Request $request)
-    {
-        $validated = $request->validate([
-            'nombre' => 'required',
-            'email' => 'required',
-            'password' => 'required',
-=======
 use App\Models\Cuenta;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -27,20 +12,13 @@ class CuentaController extends Controller
             'nombre' => 'required',
             'email' =>  'required',
             'password' => 'required'
->>>>>>> refs/remotes/origin/main
             ]); 
             $usuario = new cuenta;
             $usuario->nombre = $request->nombre;
             $usuario->email = $request->email;
             $usuario->password = $request->password;
-<<<<<<< HEAD
-            
-            $usuario->save();
-            return back()-> with('success', 'formulario validado correctamente');
-=======
             $usuario->save();
             return redirect('/')-> with('success', 'formulario validado correctamente');
->>>>>>> refs/remotes/origin/main
 
         }
         public function registro()
@@ -54,7 +32,3 @@ class CuentaController extends Controller
            return \view('pages.pagina');
        }
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> refs/remotes/origin/main

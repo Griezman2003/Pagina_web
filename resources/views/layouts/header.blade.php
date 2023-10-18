27f11af45a1dd22b405c7e9dbf00ae96bd9f9418
Griezman2003 <garciagomezgamaliel@gmail.com>
2023-10-18 13:14:27 -0600

    CORRIGIENDO README

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,7 +13,7 @@
     <a class="nav-link" href="#">Informacion</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Contacto</a>
+    <a class="nav-link" href="{{route('contacto')}}">Contacto</a>
   </li>
   <li class="nav-item">
     <a class="nav-link active" aria-current="page" href="{{ route('registro') }}">Registro</a>

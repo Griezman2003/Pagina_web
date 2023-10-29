a030c3cbf14258d8c2d801f387b0aeda7a0a99f0
Griezman2003 <garciagomezgamaliel@gmail.com>
2023-10-28 23:43:21 -0600

    textos de acuerdo a sus targetas

--- resources/views/layouts/cuerpo.blade.php
+++ resources/views/layouts/cuerpo.blade.php
@@ -26,7 +26,7 @@
     <img src="https://i.blogs.es/09c084/roblox-juegos/450_1000.webp" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Roblox</h5>
-      <p class="card-text">Esta es una tarjeta más amplia con texto de apoyo a continuación como introducción natural a contenido adicional. Este contenido es un poco más largo.</p>
+      <p class="card-text">Roblox es una plataforma de videojuegos en línea y de creación de videojuegos en línea donde los usuarios pueden crear y compartir sus propios mundos virtuales con el sistema de creación de juegos.</p>
       <p class="card-text"><small class="text-muted">Última actualización hace 3 minutos</small></p>
     </div>
   </div>
@@ -34,7 +34,7 @@
     <img src="https://interpret.la/wp-content/uploads/2022/10/MOBILE-CE-Call-of-Duty-Warzone-Mobile-will-connect-the-entire-Call-of-Duty-ecosystem.jpg" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Call of Duty Warzone</h5>
-      <p class="card-text">Esta tarjeta tiene texto de apoyo a continuación como una introducción natural a contenido adicional.</p>
+      <p class="card-text">Call of Duty es una serie de videojuegos de disparos en primera persona, de estilo bélico, desarrollada principal e inicialmente por Infinity Ward, Treyarch, Sledgehammer Games y en menor proporción Raven Software y distribuida por Activision.</p>
       <p class="card-text"><small class="text-muted">Última actualización hace 3 minutos</small></p>
     </div>
   </div>
@@ -42,7 +42,7 @@
     <img src="https://www.mundodeportivo.com/alfabeta/hero/2020/11/Guia-completa-de-League-of-Legends-trucos-y-secretos.jpg?width=768&aspect_ratio=16:9&format=nowebp" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">League of Legends</h5>
-      <p class="card-text">Esta es una tarjeta más amplia con texto de apoyo a continuación como introducción natural a contenido adicional. Esta tarjeta tiene un contenido aún más largo que la primera para mostrar esa acción de igual altura.</p>
+      <p class="card-text">League of Legends, es un videojuego multijugador de arena de batalla en línea desarrollado y publicado por Riot Games. Inspirándose en Defense of the Ancients, un mapa personalizado para Warcraft III.</p>
       <p class="card-text"><small class="text-muted">Última actualización hace 3 minutos</small></p>
     </div>
   </div>
@@ -50,26 +50,26 @@
 
 <div class="card-group">
   <div class="card">
-    <img src="https://i.blogs.es/09c084/roblox-juegos/450_1000.webp" class="card-img-top" alt="...">
+    <img src="https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_1240/b_white/f_auto/q_auto/ncom/software/switch/70010000000964/811461b8d1cacf1f2da791b478dccfe2a55457780364c3d5a95fbfcdd4c3086f" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Roblox</h5>
-      <p class="card-text">Esta es una tarjeta más amplia con texto de apoyo a continuación como introducción natural a contenido adicional. Este contenido es un poco más largo.</p>
+      <h5 class="card-title">Minecraft</h5>
+      <p class="card-text">Minecraft es un juego que consiste en colocar bloques y aventurarse. Explora mundos generados al azar y construye cosas increíbles.</p>
       <p class="card-text"><small class="text-muted">Última actualización hace 3 minutos</small></p>
     </div>
   </div>
   <div class="card">
-    <img src="https://interpret.la/wp-content/uploads/2022/10/MOBILE-CE-Call-of-Duty-Warzone-Mobile-will-connect-the-entire-Call-of-Duty-ecosystem.jpg" class="card-img-top" alt="...">
+    <img src="https://i.ytimg.com/vi/uWsQ5IWVilM/maxresdefault.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Call of Duty Warzone</h5>
-      <p class="card-text">Esta tarjeta tiene texto de apoyo a continuación como una introducción natural a contenido adicional.</p>
+      <h5 class="card-title">Clash Royale</h5>
+      <p class="card-text">Clash Royale es un videojuego de estrategia en línea para dispositivos móviles, creado por la compañía finlandesa Supercell.</p>
       <p class="card-text"><small class="text-muted">Última actualización hace 3 minutos</small></p>
     </div>
   </div>
   <div class="card">
-    <img src="https://www.mundodeportivo.com/alfabeta/hero/2020/11/Guia-completa-de-League-of-Legends-trucos-y-secretos.jpg?width=768&aspect_ratio=16:9&format=nowebp" class="card-img-top" alt="...">
+    <img src="https://www.wowchakra.com/wp-content/uploads/2020/04/WowChakraCambiosClasePaladin.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">League of Legends</h5>
-      <p class="card-text">Esta es una tarjeta más amplia con texto de apoyo a continuación como introducción natural a contenido adicional. Esta tarjeta tiene un contenido aún más largo que la primera para mostrar esa acción de igual altura.</p>
+      <h5 class="card-title">Paladins</h5>
+      <p class="card-text">Paladins: Champions of the Realm es un videojuego gratuito en línea de disparos en primera persona con componentes de hero shooter.</p>
       <p class="card-text"><small class="text-muted">Última actualización hace 3 minutos</small></p>
     </div>
   </div>
